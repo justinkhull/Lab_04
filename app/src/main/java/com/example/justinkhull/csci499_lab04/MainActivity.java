8674fb03a6d330f635dec7ce51eab4d5bb941778
justinkhull <hullj1@g.cofc.edu>
2018-03-08 18:03:42 -0500

    webview lab

--- app/src/main/java/com/example/justinkhull/csci499_lab04/MainActivity.java
+++ app/src/main/java/com/example/justinkhull/csci499_lab04/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.justinkhull.csci499_lab04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("http://www.motionlessinwhite.net");
     }
 }
