c557b3dae5c17621b7817805f975eabd00420805
justinkhull <hullj1@g.cofc.edu>
2018-03-08 18:31:17 -0500

    url connection lab

--- app/src/main/java/com/example/justinkhull/csci499_lab04/MainActivity.java
+++ app/src/main/java/com/example/justinkhull/csci499_lab04/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.justinkhull.csci499_lab04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            myMethod("https://jsonplaceholder.typicode.com/users");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected String myMethod (String myString) throws IOException {
+
+
+        URL myUrl = new URL(myString);
+        URLConnection myConnection = myUrl.openConnection();
+
+        //InputStreamReader isr = new InputStreamReader(new BufferedReader());
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //String newString = br.readLine();
+        InputStreamReader isr = new InputStreamReader((myConnection.getInputStream()));
+        BufferedReader br = new BufferedReader(isr);
+        String newString = br.readLine();
+
+        Log.i("String", "HERE");
+
+        Log.i("String", newString);
+        return myString;
     }
 }
